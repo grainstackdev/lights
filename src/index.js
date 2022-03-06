69b649af23e8f425b71b75d4ad1ccd67596a92cd
loopt-mkv <99065516+loopt-mkv@users.noreply.github.com>
2022-03-06 13:43:09 -0800

    control lights

--- src/index.js
+++ src/index.js
@@ -1,36 +1,96 @@
+// @flow
 
 import dotenvDefaults from "dotenv-defaults"
 import axios from "axios";
 import readline from 'readline-sync'
+// $FlowFixMe
 import chalk from 'chalk'
 import env from 'parsenv'
 
 dotenvDefaults.config()
 
+type Light = {
+  state: LightState,
+  name: string,
+}
+
+type LightState = {
+  on: boolean,
+  bri: number, // [0, 254]
+  hue: number, // [0, 65535]
+  sat: number, // [0, 254]
+  effect: string,
+  xy: Array<any>,
+  ct: number,
+  alert: string,
+  colormode: string,
+  mode: string,
+  reachable: boolean
+}
+
 const {
   USERNAME
 } = process.env
 
-let username = USERNAME
+let username = USERNAME || ''
 
 Promise.resolve().then(async () => {
   const res = await axios.get('https://discovery.meethue.com/')
   const bridgeIp = res.data?.[0]?.internalipaddress
   const bridgeUrl = `http://${bridgeIp}`
 
-  const helper = make(bridgeUrl)
+  const bridge = makeBridge(bridgeUrl)
 
-  const userExists = await helper.checkUser(username)
+  const userExists = await bridge.checkUser(username)
   if (!userExists) {
     console.log(chalk.bgRed.bold('\n  Error  '))
     console.log('The user specified by .env does not exist. Creating a new user user...')
-    await helper.createUser()
+    await bridge.createUser()
   }
 
+  const lights = await bridge.listLights()
+  const lightId = Object.keys(lights)[0]
+  if (!lightId) {
+    throw new Error('Unable to find any light')
+  }
+
+  await bridge.setLightState(lightId, {on: true})
+
+  const nColors = 12
+  const cycleTime = 15000
+  const timeStep = 200
+  const stepRatio = timeStep / cycleTime
+
+  const hueFullStep = 65535 / nColors
+  const hueTimeStep = hueFullStep * stepRatio
+  const briFullStep = cycleTime / 2
+  const briTimeStep = briFullStep * stepRatio
+
+
+  let lerp = 0
+  let hue = 0
+  setInterval(async () => {
+    lerp += briTimeStep * (2 * Math.PI) / briFullStep / 2
+    const bri3 = Math.round(Math.cos(lerp) * Math.cos(lerp) * 254)
+    const bri4 = Math.round(Math.sin(lerp) * Math.sin(lerp) * 254)
+    await bridge.setLightState('3', {bri: bri3})
+    await bridge.setLightState('4', {bri: bri4})
+
+    await bridge.setLightState('3', {hue})
+    await bridge.setLightState('4', {hue})
+    // await bridge.setLightState('4', {hue: (65535 - hue)})
+
+    // await bridge.setLightState('3', {bri})
+    // await bridge.setLightState('4', {bri: (254 - bri)})
+
+
+
+    hue = Math.floor((hue + hueTimeStep) % 65535)
+  }, timeStep)
 
 })
 
-function make(bridgeUrl) {
+function makeBridge(bridgeUrl) {
   const instance = axios.create({
     baseURL: bridgeUrl
   })
@@ -81,12 +141,23 @@ function make(bridgeUrl) {
       console.log(chalk.bgGreen.bold('\n  Created  '))
       console.log('A new user was created and saved into .env.')
     }
-    // console.log('username', username)
+  }
+
+  async function listLights(): Promise<{[lightId: string]: Light}> {
+    const res = await instance.get(`/api/${username}/lights`)
+    return res.data || {}
+  }
+
+  async function setLightState(lightId: string, state: $Shape<LightState>) {
+    const res = await instance.put(`/api/${username}/lights/${lightId}/state`, state)
+    console.log('res.data', res.data)
   }
 
   return {
     checkUser,
-    createUser
+    createUser,
+    listLights,
+    setLightState
   }
 }
 
